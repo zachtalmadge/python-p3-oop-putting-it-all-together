09086bfa6f9aa3de7cf785b9214de9a9ad77f15d
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-24 17:53:37 -0400

    pass tests

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,22 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
+    def __init__(self, title, page_count):
+        self.title = title
+        self._page_count = page_count  # set the internal attribute directly to avoid the setter
+
+    @property
+    def page_count(self):
+        """The page_count property"""
+        return self._page_count
         
+    @page_count.setter
+    def page_count(self, page_count):
+        """page count must be an integer"""
+        if not isinstance(page_count, int):
+            print('page_count must be an integer')
+        else:
+            self._page_count = page_count
+            
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        
+    @property
+    def size(self):
+        """Return the size of the Shot"""
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        """Size must be an integer"""
+        if not isinstance(size, int):
+            print('size must be an integer')
+            
+        else:
+            self._size = size
+            
+    def cobble(self):
+        print('Your shoe is as good as new!')
+        self.condition = "New"
